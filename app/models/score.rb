d99ac9fc005bb924ad0e0c8f45a57e4747bffffc
Rumen Milushev <r@vert.bg>
2019-01-23 01:29:37 +0200

    Improve score displaying

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,3 +1,6 @@
 class Score < ApplicationRecord
   belongs_to :tournament
+  belongs_to :player_one, class_name: 'Player'
+  belongs_to :player_two, class_name: 'Player'
+  belongs_to :winner, class_name: 'Player'
 end
